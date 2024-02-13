4e6a9ae753fde169c2b51a9426c4327e8e16b10b
JRisco05 <oscanova05@gmail.com>
2024-02-13 02:37:45 -0500

    eliminando cod innecesario

--- src/app/dashboard/rinfa/editarRinfa/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function editarRinfa() {
-    return (
-        <div>editarRinfa</div>
-    )
-}
--- src/app/dashboard/rinfa/subirRinfa/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function subirRinfa() {
-  return (
-    <div>subirRinfa</div>
-  )
-}
