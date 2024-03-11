77f01e4bf81e47ee7a7cdf22e49708a2ea790c20
JRisco05 <oscanova05@gmail.com>
2024-03-11 07:41:33 -0500

    terminado primera matriz

--- src/app/dashboard/mapa/page.jsx
+++ src/app/dashboard/mapa/page.jsx
@@ -1,5 +1,6 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Chart as ChartJS, CategoryScale, LinearScale } from 'chart.js';
 import { MatrixController, MatrixElement } from 'chartjs-chart-matrix';
 import { Chart } from 'react-chartjs-2';
@@ -12,71 +13,94 @@ ChartJS.register(
   MatrixElement
 );
 
-// Datos de tu gráfico matrix
-const data = {
-  datasets: [
-    {
-      label: 'My Matrix',
-      data: [
-        { x: 'Enero', y: 'Alto', v: 11 },
-        { x: 'Febrero', y: 'Intermedio', v: 12 },
-        { x: 'Marzo', y: 'Bajo', v: 13 },
-        { x: 'Abril', y: 'Bajo', v: 21 },
-        { x: 'Mayo', y: 'Intermedio', v: 22 },
-        { x: 'Junio', y: 'Alto', v: 23 },
-        { x: 'Julio', y: 'Alto', v: 31 },
-        { x: 'Agosto', y: 'Alto', v: 32 },
-        { x: 'Septiembre', y: 'Bajo', v: 33 },
-        { x: 'Agosto', y: 'Intermedio', v: 20 },
-        { x: 'Setpiembre', y: 'Intermedio', v: 21 },
-        { x: 'Octubre', y: 'Bajo', v: 22 },
-        { x: 'Noviembre', y: 'Bajo', v: 23 },
-        { x: 'Diciembre', y: 'Alto', v: 24 },
-        
-        // ... más datos aquí
-      ],
-      backgroundColor(context) {
-        const value = context.dataset.data[context.dataIndex].v;
-        const alpha = Math.max(0, Math.min(1, (value - 5) / 40)); // Aseguramos que alpha esté entre 0 y 1
-        return `rgba(0, 128, 0, ${alpha})`; // Ajusta los valores RGB para verde y usa el alpha calculado
-      },
-      // ... más configuración aquí
-    },
-  ],
-  // ... más configuración aquí
-};
-
-const options = {
-  scales: {
-    x: {
-      // Se configura la escala del eje X para utilizar las etiquetas 'A', 'B', 'C'
-      type: 'category',
-      labels: ['Enero', 'febrero', 'Marzo'],
-    },
-    y: {
-      // Se configura la escala del eje Y para utilizar las etiquetas 'X', 'Y', 'Z'
-      type: 'category',
-      labels: ['Alto', 'Intermedio', 'Bajo'],
-      offset: true,
-    },
-  },
-  plugins: {
-    tooltip: {
-      callbacks: {
-        title() {
-          return '';
-        },
-        label(context) {
-          const v = context.raw;
-          return `x: ${v.x}, y: ${v.y}, v: ${v.v}`;
-        },
-      },
-    },
-  },
-  // ... más opciones si son necesarias
+const mapearNumeroMesANombre = (numeroMes) => {
+  const nombresDeMes = [
+    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+  ];
+  return nombresDeMes[numeroMes - 1];
 };
 
 export default function Example() {
+  const [riesgosMes, setRiesgosMes] = useState([]);
+
+  useEffect(() => {
+    const getRiesgoMes = async () => {
+      try {
+        const response = await axios.get('/api/conflicto/riesgoMes');
+        setRiesgosMes(response.data); // Asumiendo que la respuesta es un arreglo
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+
+    getRiesgoMes();
+  }, []);
+
+  // Acumulación de datos
+  const datosAcumulados = riesgosMes.reduce((acc, { mes, riesgo, cantidad }) => {
+    // Creamos una clave única para cada combinación de mes y riesgo
+    const clave = `${mapearNumeroMesANombre(mes)}-${riesgo}`;
+    // Si ya existe, sumamos a la cantidad existente
+    if (acc[clave]) {
+      acc[clave].v += cantidad;
+    } else {
+      // Si no existe, creamos una nueva entrada
+      acc[clave] = { x: mapearNumeroMesANombre(mes), y: riesgo, v: cantidad };
+    }
+    return acc;
+  }, {});
+
+  // Convertimos el objeto acumulativo en un array para el gráfico
+  const datosGrafico = Object.values(datosAcumulados);
+
+  // Configuración de los datos para Chart.js
+  const data = {
+    datasets: [
+      {
+        label: 'Cantidad de eventos por nivel de riesgo y mes',
+        data: datosGrafico,
+        backgroundColor: function (context) {
+          const data = context.dataset.data[context.dataIndex];
+          if (!data) {
+            return 'rgba(0, 128, 0, 0.5)'; // Retornar un color por defecto si no hay datos.
+          }
+          const value = data.v;
+          const alpha = Math.max(0, Math.min(1, (value - 5) / 40));
+          return `rgba(0, 128, 0, ${alpha})`;
+        },
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: 'category',
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        offset: true,
+      },
+      y: {
+        type: 'category',
+        labels: ['Alto', 'Intermedio', 'Bajo'],
+        offset: true,
+      },
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          title() {
+            return ''; // No title
+          },
+          label(context) {
+            const value = context.dataset.data[context.dataIndex]?.v;
+            return `Cantidad: ${value}`; // Muestra la cantidad, o nada si es undefined
+          },
+        },
+      },
+    },
+  };
+
   return (
     <div className='ml-64 mt-24'>
       <Chart type='matrix' data={data} options={options} />
